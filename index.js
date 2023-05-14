3c7142a539033126e2517f8e4a2a898883bd1591
HAlGhanim <imzaji98@gmail.com>
2023-05-14 19:06:19 +0300

    extra done

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const numbers = [1, 2, 3, 4, 5];
 // write code to create a new array where each
 // number is multiplied by 2 using the map method.
 // ============== Your answer here:
-
+console.log(numbers.map(multi => multi * 2));
 // ==============
 
 // Q2)
@@ -13,7 +13,7 @@ const names = ["Alice", "Bob", "Charlie"];
 // write code to create a new array where each name is
 // converted to uppercase using the map method.
 // ============== Your answer here:
-
+console.log(names.map(upper => upper.toUpperCase()));
 // ==============
 
 // Q3)
@@ -22,7 +22,7 @@ const numbers2 = [1, 4, 9, 16, 25];
 // write code to create a new array where each
 //  number is squared using the map method.
 // ============== Your answer here:
-
+console.log(numbers2.map(squared => squared ** 2));
 // ==============
 
 // Q4)
@@ -31,7 +31,7 @@ const names2 = ["John", "Jane", "Tom"];
 // write code to create a new array where each name
 // is appended with " Smith" using the map method.
 // ============== Your answer here:
-
+console.log(names2.map(surName => surName + " Smith"));
 // ==============
 
 // Q5)
@@ -40,7 +40,7 @@ const numbers3 = [2, 4, 6, 8, 10];
 //  write code to create a new array where each
 // number is divided by 2 using the map method.
 // ============== Your answer here:
-
+console.log(numbers3.map(divide => divide / 2));
 // ==============
 
 // Q6)
@@ -49,5 +49,5 @@ const fruits = ["apple", "banana", "orange"];
 //  write code to create a new array where each fruit
 //  name is capitalized using the map method. Ex, Apple ...
 // ============== Your answer here:
-
+console.log(fruits.map(cap => cap[0].toUpperCase() + cap.slice(1)));
 // ==============
